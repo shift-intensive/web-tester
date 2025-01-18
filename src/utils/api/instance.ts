43236d08057ac790447cea377d5eabdbb8756faf
debabin <dmitriybabinup@gmail.com>
2025-01-18 14:22:38 +0700

    main 🧊 update deps

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { LOCAL_STORAGE_KEYS } from '../constants';
 
 export const api = axios.create({
-  baseURL: 'http://shift-intensive.ru',
+  baseURL: 'https://shift-intensive.ru',
   validateStatus: () => true,
   headers: {
     tester: true
